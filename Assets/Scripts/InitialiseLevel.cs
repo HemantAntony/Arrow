b423e101e6fcbce438f2b4090fcaad83257f1bf4
Hemant Antony <88136054+HemantAntony@users.noreply.github.com>
2023-11-02 17:36:32 +0530

    Implemented level 6 and changed player bounds construction algorithm

--- Assets/Scripts/InitialiseLevel.cs
+++ Assets/Scripts/InitialiseLevel.cs
@@ -1,26 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class InitialiseLevel : MonoBehaviour
 {
     [SerializeField] private GameObject playerPrefab;
-    [SerializeField] private Vector2 playerPositionOffset;
     [SerializeField] private GameObject endPrefab;
-    [SerializeField] private Vector2 endPositionOffset;
     [SerializeField] private GameObject arrowBounds;
+    [SerializeField] private Transform ground;
+
+    private Vector2 playerPositionOffset = new Vector2(1, 2);
+    private float endPositionOffset = -1;
+
     void Start()
     {
+        if (Camera.main.GetComponent<CameraMovement>() == null)
+        {
+            Vector2 scale = ground.localScale;
+            scale.x = (Camera.main.ViewportToWorldPoint(new Vector2(1, 1)) - Camera.main.ViewportToWorldPoint(new Vector2(0, 0))).x;
+            ground.localScale = scale;
+            PlayerBounds.instance.UpdatePlayerBounds();
+        }
+
         Instantiate(playerPrefab, Camera.main.ViewportToWorldPoint(new Vector2(0, 0))
             + playerPrefab.transform.localScale / 2
             + (Vector3)playerPositionOffset, Quaternion.identity);
-        
-        if (endPrefab != null )
-        {
-            Instantiate(endPrefab, (Vector2)Camera.main.ViewportToWorldPoint(new Vector3(1, 0))
-                + new Vector2(-endPrefab.transform.localScale.x / 2, endPrefab.transform.localScale.y / 2)
-                + endPositionOffset, Quaternion.identity);
-        }
+        float x = ground.position.x + ground.localScale.x / 2 - endPrefab.transform.localScale.x / 2 + endPositionOffset;
+        float y = ground.position.y + ground.localScale.y / 2 + endPrefab.transform.localScale.y / 2;
+        Instantiate(endPrefab, new Vector2(x, y), Quaternion.identity);
 
         CameraMovement cameraMovement = Camera.main.GetComponent<CameraMovement>();
         if (cameraMovement != null)
--- Assets/Scripts/PlayerBounds.cs
+++ Assets/Scripts/PlayerBounds.cs
@@ -3,30 +3,47 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class Wall : MonoBehaviour
+public class PlayerBounds : MonoBehaviour
 {
     [SerializeField] private GameObject wallPrefab;
 
-    void Start()
+    internal static PlayerBounds instance;
+
+    private List<Transform> playerBounds = new();
+
+    public void UpdatePlayerBounds()
     {
+        foreach(Transform bound in playerBounds)
+        {
+            Destroy(bound.gameObject);
+        }
+
         Transform ground = GameObject.FindGameObjectWithTag("Ground").transform;
         Vector2 start = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
-        Vector2 end = new Vector2( ground.position.x + ground.localScale.x / 2,
+        Vector2 end = new Vector2(ground.position.x + ground.localScale.x / 2,
                         Camera.main.ViewportToWorldPoint(new Vector2(1, 1)).y);
 
         Transform wall = Instantiate(wallPrefab, Vector2.zero, Quaternion.identity).transform;
         wall.localScale = new Vector2(end.x - start.x + 1f, 1f);
         wall.position = new Vector2((end.x + start.x) / 2, end.y + wall.transform.localScale.y / 2);
         wall.parent = transform;
+        playerBounds.Add(wall);
 
         wall = Instantiate(wallPrefab, Vector2.zero, Quaternion.identity).transform;
         wall.localScale = new Vector2(1f, end.y - start.y + 1f);
         wall.position = new Vector2(start.x - wall.transform.localScale.x / 2, (start.y + end.y) / 2);
         wall.parent = transform;
+        playerBounds.Add(wall);
 
         wall = Instantiate(wallPrefab, Vector2.zero, Quaternion.identity).transform;
         wall.localScale = new Vector2(1f, end.y - start.y + 1f);
         wall.position = new Vector2(end.x + wall.transform.localScale.x / 2, (start.y + end.y) / 2);
         wall.parent = transform;
+        playerBounds.Add(wall);
+    }
+
+    void Start()
+    {
+        instance = this;
     }
 }
